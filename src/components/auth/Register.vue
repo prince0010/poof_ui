909f162c0f0222f5530d5b7375440ed733201b1c
PRINCE <princenagac12@gmail.com>
2024-09-20 00:07:51 +0800

    Adding a validation in the register.vue fields and added the inputs in the register.vue in the inputs

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -5,35 +5,75 @@
         <form @submit.prevent="handleRegister">
             <div class="form-group">
                 <label for="fname">First Name</label>
-                <input type="text" id="fname" v-model="fname">
+                <input
+                    type="text" 
+                    id="fname" 
+                    v-model="fname"
+                    @input="validatedFirstName"
+                    >
             </div>
             <div class="form-group">
                 <label for="mname">Middle Name</label>
-                <input type="text" id="mname" v-model="mname">
+                <input
+                    type="text" 
+                    id="mname" 
+                    v-model="mname"
+                    @input="validatedMiddleName"
+                    >
             </div>
             <div class="form-group">
                 <label for="lname">Last Name</label>
-                <input type="text" id="lname" v-model="lname">
+                <input
+                    type="text" 
+                    id="lname" 
+                    v-model="lname"
+                    @input="validatedLastName"
+                    >
             </div>
             <div class="form-group">
                 <label for="address">Address</label>
-                <input type="text" id="address" v-model="address">
+                <input
+                    type="text" 
+                    id="address
+                    "v-model="address"
+                    @input="validatedAddress"
+                    >
             </div>
             <div class="form-group">
                 <label for="phonenum">Phone Number</label>
-                <input type="text" id="phonenum" v-model="phonenum">
+                <input
+                    type="text" 
+                    id="phonenu
+                    m" v-model="phonenum"
+                    @input="validatedPhoneNumber"
+                    >
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" v-model="email">
+                <input
+                    type="email"
+                     id="email"
+                      v-model="email"
+                      @input="validatedEmailField"
+                      >
             </div>
             <div class="form-group">
                 <label for="passsword">Password</label>
-                <input type="text" id="passsword" v-model="passsword">
+                <input
+                    type="text" 
+                    id="passsword" 
+                    v-model="password"
+                    @input="validatedPasswordField"
+                    >
             </div>
             <div class="form-group">
                 <label for="cpassword">Confirm Password</label>
-                <input type="text" id="cpassword" v-model="cpassword">
+                <input
+                    type="text" 
+                    id="cpasswo
+                    rd" v-model="cpassword"
+                    @input="validateConfirmPasswordField"
+                    >
             </div>
 
             <button type="submit">Register</button>
@@ -53,7 +93,7 @@ import { ref } from 'vue';
     const address = ref('');
     const phonenum = ref('');
     const email = ref('');
-    const passsword = ref('');
+    const password = ref('');
     const cpassword = ref('');
     const errors = ref({
         fname: '',
@@ -62,7 +102,7 @@ import { ref } from 'vue';
         address: '',
         phonenum: '',
         email: '',
-        passsword: '',
+        password: '',
         cpassword: '',
     });
     const validatedFirstName = () => {
@@ -111,18 +151,59 @@ import { ref } from 'vue';
         }
     };
     const validatedEmailField = () => {
-        if(!validatedEmail(email.value))
+        if(!email.value){
+            errors.value.email = 'Email is Required';
+        }
+        else if(!validatedEmail(email.value))
         {
-
+            errors.value.email = 'Validated Email is Required';
+        }
+        else {
+            errors.value.email = '';
         }
     };
+    const validatedPasswordField = () => {
+        if(!password.value){
+            errors.value.password = 'Password is Required';
+        }
+        else if(password.value !== cpassword.value){
+            errors.value.password = 'Password does not match';
+        }
+        else {
+            errors.value.password = '';
+        }
+    };
+
+    const validateConfirmPasswordField = () => {
+        if(!cpassword.value){
+            errors.value.cpassword = 'Confirm Password is Required';
+        }
+        else if(cpassword.value !== password.value){
+            errors.value.cpassword = 'Password does not match';
+        }
+        else {
+            errors.value.cpassword = '';
+        }
+    }
     const handleRegister = () => {
 
     }
 
+    // Validated Email Validation function simple regex check
     const validatedEmail = (email: string) =>{
         const re = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
         return re.test(email);
     };
 
+    // Add a Validation for a phone number field 
+
+    // Add a Validation for a address field
+
 </script>
+
+
+
+<!-- CSS STYLE -->
+<style>
+
+</style>
